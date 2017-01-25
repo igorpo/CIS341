625c5fd0c2dc84604968a9646f145d1d8ff0ebe3
Igor Pogorelskiy <ipogorelskiy@gmail.com>
2017-01-25 16:12:37 -0500

    Added makefile and did mem_addr

--- hw2/simulator.ml
+++ hw2/simulator.ml
@@ -136,7 +136,8 @@ let interp_cnd {fo; fs; fz} : cnd -> bool = fun x -> failwith "interp_cnd unimpl
 (* Maps an X86lite address into Some OCaml array index,
    or None if the address is not within the legal address space. *)
 let map_addr (addr:quad) : int option =
-  failwith "map_addr not implemented"
+  let addr_check = addr >= mem_bot && addr <= mem_top in
+  if addr_check then Some (Int64.to_int (Int64.sub addr mem_bot)) else None 
 
 (* Simulates one step of the machine:
     - fetch the instruction at %rip
