0b392af9b4dcc0d6c098c04e714bec12d8cf26da
Yagil <yagilbu@gmail.com>
2017-02-01 20:23:26 -0500

    possibly leaq working

--- hw2/simulator.ml
+++ hw2/simulator.ml
@@ -355,8 +355,15 @@ let update_flags (f:flags) (fo:bool) (fs:bool) (fz:bool) : unit =
   f.fo <- fo; f.fs <- fs; f.fz <- fz    
 
 
-let leaq : unit =
-  ()
+let leaq (m:mach) (o:operand list): unit =
+  begin match o with
+    |s::d::[] ->
+      begin match s with
+      | Imm _ | Reg _ -> failwith "Can only be called with Ind"
+      | _ -> let v = get_val_from_loc m s in set_val_in_loc v d m 
+      end
+    | _ -> failwith "Wrong number of operands for leaq"
+  end
 
 let c_jump : unit =
   ()
@@ -498,7 +505,10 @@ let exec_ins (inst:ins) (m:mach) : unit =
   | Retq -> 
     Printf.printf "OP === Retq\n";
     return m;
-  | Leaq -> () (* SRC DEST *)
+    rip_incr m;
+  | Leaq -> 
+    leaq m oprnd_list;
+    rip_incr m;
   | J j -> () (* CC, DEST *)
   | Set s -> () (* CC, DEST *)
   | Callq -> () (* SRC DEST *)
@@ -627,7 +637,6 @@ let handle_text (m:map * sbyte list) (e:elem) : (map * sbyte list) =
   begin match e.asm with
   (* fold on map, t with patch_ins*)
   | Text t -> 
-    let label = e.lbl in 
     let new_new_map, patched_ins = List.fold_left patch_ins (_map, text_seg) t in
     (new_new_map, patched_ins)
   | _ -> m
