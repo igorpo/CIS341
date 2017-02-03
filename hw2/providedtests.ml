e07daaf4dd2f411e6986aff396608b6918b1fb15
Igor Pogorelskiy <ipogorelskiy@gmail.com>
2017-02-02 21:27:10 -0500

    almost

--- hw2/providedtests.ml
+++ hw2/providedtests.ml
@@ -24,28 +24,31 @@ let reverse a =
       ; text "reverse"
           [ Cmpq,  [~%R08; ~$0] 
           ; J Eq,  [~$$"return"]
+          ; Movq,  [~$0; ~%R11] (* R11 is counter *)
+          ; Movq,  [~%R08; ~%R12]
           ; Callq, [~$$"divide_by_10"] 
+          ; Movq,  [~%R08; ~%R10]
           ; Callq, [~$$"mod_10"] 
           ; Imulq, [~$10; ~%R09] (* multiplies acc by 10 *)
           ; Addq,  [~%R10; ~%R09]
-          ; Callq, [~$$"reverse"]
+          ; Movq,  [~%R12; ~%R08]
+          ; Jmp,   [~$$"reverse"]
           ]
       ; text "divide_by_10"     (* divides R08 by 10, puts it R11*)
-       	  [ Movq,  [~$0; ~%R11] (* R11 is counter *)
-          ; Cmpq,  [~%R08; ~$10]
+       	  [ Cmpq,  [~%R12; ~$10]
           ; J Lt,  [~$$"div_ret"]
-          ; Subq,  [~$10; ~%R08]
+          ; Subq,  [~$10; ~%R12]
           ; Addq,  [~$1; ~%R11]
+          ; Jmp,   [~$$"divide_by_10"]
           ]
       ; text "mod_10"            (* stores R08 % 10 in R10 *)
-       	  [ Movq,  [~%R08; ~%R10]
-       	  ; Cmpq,  [~%R10; ~$10]
+       	  [ Cmpq,  [~%R10; ~$10]
           ; J Lt,  [~$$"mod_ret"]
-          ; Subq,  [~%R10; ~$10]
+          ; Subq,  [~$10; ~%R10]
           ; Jmp,   [~$$"mod_10"]
           ]
       ; text "div_ret"
-      	  [ Movq,  [~%R11; ~%R08]
+      	  [ Movq,  [~%R11; ~%R12]
       	  ; Retq,  []
       	  ]
       ; text "mod_ret"
