dd6acc9b87c7df3901513290140efb7f25872284
Igor Pogorelskiy <ipogorelskiy@gmail.com>
2017-03-26 22:41:50 -0400

    for almost done

--- hw4/frontend.ml
+++ hw4/frontend.ml
@@ -261,26 +261,20 @@ let rec cmp_exp (c:Ctxt.t) (exp:Ast.exp node) : Ll.ty * Ll.operand * stream =
   | CBool b -> (Ll.I1, Ll.Const (bool_to_int64 b),[])
   | CInt i -> (Ll.I64, Ll.Const i,[])
   | CStr s -> 
-     
      let str_gid = gensym "string" in
      let typ = Ll.Array ((String.length s) + 1, Ll.I8) in
      let str_ginit = Ll.GString s in
      let str_gdecl = (typ, str_ginit) in
      (typ, Ll.Gid str_gid, [G (str_gid, str_gdecl)])
-
   | CArr (typ, exp_node_list) -> failwith "CArr"
   | NewArr (typ, exp_node) -> failwith "NewArr"
   | Id i -> 
-    (* Printf.printf "Id %s \n\n\n" i; *)
     let typ, opr = Ctxt.lookup i c in
-    (* (typ, opr, []) *)
-    (* Printf.printf "Id %s's type is " i; *)
-
     begin match opr with
-    | Gid g -> (* Printf.printf "Global\n"; *) (typ, Ll.Gid g, [])
-    | Id id -> (* Printf.printf "Local\n";  *)(typ, Ll.Id id, [])
-    | Const c -> (* Printf.printf "Const\n"; *) (typ, Ll.Id i, [])
-    | Null -> (* Printf.printf "Null\n"; *) (typ, Ll.Id i, [])
+    | Gid g -> (typ, Ll.Gid g, [])
+    | Id id -> (typ, Ll.Id id, [])
+    | Const c -> (typ, Ll.Id i, [])
+    | Null -> (typ, Ll.Id i, [])
     end
     
   | Index (exp_node1, exp_node2) -> failwith "Index" 
@@ -288,7 +282,6 @@ let rec cmp_exp (c:Ctxt.t) (exp:Ast.exp node) : Ll.ty * Ll.operand * stream =
     let (ty_list, typ), ll_op = Ctxt.lookup_function i c in
     let _, ty_op_list, streams = List.fold_left2 zip_args_w_type (c,[], []) ty_list exp_node_list in
     let lbl = gensym i in
-    (* Printf.printf "Trying to compile Call %s and lbl=%s\n" i lbl;  *)
     (typ, Ll.Id lbl, streams >@ [I (lbl, Ll.Call (typ, ll_op, ty_op_list))])
   | Uop (uop1, e1) -> 
     let t = expr_type (Uop (uop1, e1)) in 
@@ -296,9 +289,6 @@ let rec cmp_exp (c:Ctxt.t) (exp:Ast.exp node) : Ll.ty * Ll.operand * stream =
     let typ, op, strm = load_helper @@ cmp_exp c e1 in
     (cmp_ty t, Ll.Id id, strm >@ [I (id, cmp_uop uop1 op typ)])
   end   
-
-(* and cmp_exp_as_ty : Ctxt.t -> Ast.exp node -> Ll.ty -> Ll.operand * stream =
-  failwith "" *)
 
 and load_helper (a: Ll.ty * Ll.operand * stream) : Ll.ty * Ll.operand * stream =
   (* Printf.printf "load_helper"; *)
@@ -350,9 +340,17 @@ and zip_args_w_type (c:Ctxt.t * arg_list * stream) (t:Ll.ty) (a:Ast.exp node) : 
  *)
 
 let safe_ptr_usage (t:Ll.ty) (op:Ll.operand) : Ll.operand * stream =
-  begin match op with
+  begin match op with;
     | Gid g -> let _, o, str = load_helper (t,op,[]) in (o, str)
     | _ -> (op, [])
+  end
+
+let rec for_decl_helper (c: Ctxt.t) (vd_l: vdecl list) : stream =
+  begin match vd_l with 
+  | (h_id, h_expr_n)::tl -> 
+    let _, _, strm = cmp_exp c h_expr_n in
+    strm >@ (for_decl_helper c tl)
+  | [] -> []
   end
 
 let rec cmp_stmt (c:Ctxt.t) (rt:Ll.ty) (stmt:Ast.stmt node) : Ctxt.t * stream =
@@ -439,11 +437,42 @@ let rec cmp_stmt (c:Ctxt.t) (rt:Ll.ty) (stmt:Ast.stmt node) : Ctxt.t * stream =
       >@ cmp_b2
       >@ [T (Br (l3))]
       >@ [L (l3)]
-      (* >@ [T (Br ("end"))] *)
     in 
     (c, strm)
-  | For (vd_list, exp_node_option,
-           stmt_node_option, block1) -> failwith "For"
+  | For (vd_list, cnd_option,
+           incr_option, b) -> 
+    let vd_strm = for_decl_helper c vd_list in
+
+    (* conditional section *) 
+    let cnd_ty, cnd_opr, cnd_strm = 
+    begin match cnd_option with 
+    | Some s -> cmp_exp c s 
+    | None -> cmp_exp c (Ast.no_loc (Ll.Const (Ast.CBool(true))))
+    end in 
+
+    (* incrementer section *)
+    let _, _, incr_strm =
+    begin match incr_option with 
+    | Some s -> cmp_exp c s 
+    | None -> _, _, []
+    end in 
+
+    let c_b = cmp_block c in 
+    let l0 = gensym "begin_for" in 
+    let l1 = gensym "for_block" in 
+    let l2 = gensym "end_for" in 
+    let new_op, load_strm = safe_ptr_usage typ cnd_opr in 
+    let strm = vd_strm 
+      >@ [T (Br (l0))]
+      >@ cnd_strm 
+      >@ [T (Cbr (new_op, l1, l2))]
+      >@ [L (l1)]
+      >@ c_b 
+      >@ incr_strm
+      >@ [T (Br (l0))]
+      >@ [L (l2)]
+    in  
+    (c, strm)
   | While (while_cond_exp, while_block) -> 
     let typ, while_opr, while_strm = cmp_exp c while_cond_exp in
     begin match typ with
